8204d9b0cb5074f5fcc9ef284eda9b407b24fe89
ismaldonado <isanchezm217@gmail.com>
2022-04-07 13:59:22 +0200

    Spring-mvc-example primeros pasos

---
+++ src/main/java/com/formacion/nttdata/model/User.java
@@ -0,0 +1,23 @@
+package com.formacion.nttdata.model;
+
+public class User {
+	String userName;
+	String userSurname;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserSurname() {
+		return userSurname;
+	}
+	public void setUserSurname(String userSurname) {
+		this.userSurname = userSurname;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/formacion/nttdata/servicio/HelloServicio.java
@@ -0,0 +1,13 @@
+package com.formacion.nttdata.servicio;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloServicio {
+	
+	public HelloServicio helloServicio;
+	
+	
+	
+
+}
